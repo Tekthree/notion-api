32a2b358145e233119cc6519bb810eb02c000199
Tek <tekthree.gmail.com>
2021-05-20 22:12:30 -0700

    getting data back

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 'use strict'
 
 const dotenv = require('dotenv').config();
+const {Client} = require('@notionhq/client');
 
-const express = require('express');
-const app = express();
+
 const DATABASE = process.env.NOTION_DATABASE_ID;
+
+
+// Init client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+})
+
+const getVideos = async () => {
+  const payload = {
+    path: `databases/${DATABASE}/query`,
+    method: 'POST'
+  }
+
+  const {results} = await notion.request(payload);
+
+  const schedule = results.map((page)=>{
+    console.log(page.properties.Description)
+  })
+
+  console.log(results);
+}
+
+getVideos();
+
+
