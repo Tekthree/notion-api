2d75018e1aebea7f9f5905a44b98c2b22d34923a
Tek <tekthree.gmail.com>
2021-05-22 16:07:06 -0700

    fixing deployment

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 "use strict";
-const express = require('express');
-const getVideos = require('./services/notion.js');
+const express = require("express");
+const getVideos = require("./services/notion.js");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/schedule', async (req, res) =>{
+app.get("/schedule", async (req, res) => {
   const schedule = await getVideos();
   res.json(schedule);
-})
+});
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
-
