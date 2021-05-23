9d52723c2b54c0ecfcc7f2c030f8c241bffac9ab
Tek <tekthree.gmail.com>
2021-05-23 16:06:59 -0700

    cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict";
 const express = require("express");
+const cors = require('cors');
 const getVideos = require("./services/notion.js");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 
-
+app.use(cors());
 app.use(express.static("public"));
 
 app.get("/schedule", async (req, res) => {
